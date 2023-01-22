a9e9b0dc8681c56a7bf3523a61eb8c4146973a3a
Ashutosh Tanwar <ashutoshtanwar1@gmail.com>
2023-01-23 00:32:58 +0530

    First Commit

---
+++ App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import tw, {useAppColorScheme, useDeviceContext} from 'twrnc';
+import Image from './components/atoms/Image';
+import Giphy from './components/Giphy';
+import {NAME, THEME_ICONS} from './constants';
+
+const {darker, lighter} = Colors;
+
+function App(): JSX.Element {
+  useDeviceContext(tw, {withDeviceColorScheme: false});
+
+  const [colorScheme, toggleColorScheme] = useAppColorScheme(tw);
+  const isDarkMode = colorScheme === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? darker : lighter,
+  };
+
+  return (
+    <SafeAreaView>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View style={tw`h-full bg-[${lighter}] dark:bg-[${darker}]`}>
+        <TouchableOpacity
+          onPress={toggleColorScheme}
+          style={tw`flex items-end mr-3 mt-1`}>
+          <Image
+            style={tw`h-6 w-6`}
+            source={{
+              uri: isDarkMode ? THEME_ICONS.light : THEME_ICONS.dark,
+            }}
+          />
+        </TouchableOpacity>
+        <Text
+          style={tw`tracking-3 text-[${darker}] dark:text-[${lighter}] text-center text-4xl font-black`}>
+          {NAME}
+        </Text>
+        <Giphy />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ constants/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  NAME: 'GIPHY',
+  THEME_ICONS: {
+    light: 'https://i.ibb.co/XVL6t43/index-1.png',
+    dark: 'https://i.ibb.co/2WpPDGN/index.png',
+  },
+};
